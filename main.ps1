9a7aa555ffd84baac859f9f5d9c16b99ad018185
Paulmartin <pmartin@tuta.io>
2016-05-12 20:04:42 -0500

    first commit

---
+++ main.ps1
@@ -0,0 +1,4 @@
+# PowerShell Example
+
+#[Reflection.Assembly]::LoadFile("$PSScriptRoot/foo/bar")
+#[RestSharpExample.Example1]::Example1()
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Python example
+
+import clr, os
+clr.AddReference(os.path.dirname(os.path.realpath(__file__)) + '/RestSharpExample/bin/Release/RestSharpExample.dll')
+import RestSharpExample
+
+RestSharpExample.Example1()
