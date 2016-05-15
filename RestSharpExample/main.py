aa70bce8939968fd968b975934a88e6df89ac819
Paulmartin <pmartin@tuta.io>
2016-05-15 18:10:32 -0500

    Turning this into a general scratch/junk repo

--- RestSharpExample/main.py
+++ RestSharpExample/main.py
@@ -6,4 +6,4 @@ import clr, os
 clr.AddReference(os.path.dirname(os.path.realpath(__file__)) + '/RestSharpExample/bin/Release/RestSharpExample.dll')
 import RestSharpExample
 
-RestSharpExample.Example1()
+RestSharpExample.Example.Example1()
